71cd1ccb485202560a9e8fad1d30df836eb650ab
ImamM5 <imamm@nycstudents..net>
2022-10-12 09:29:04 -0400

    new code

--- src/Runner.java
+++ src/Runner.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Runner {
+public class Runner
+{
+    public static void main(String[] args)
+    {
+        String greeting = "Good Morning";
+        int strLen = greeting.length();
+        System.out.println(strLen);
+
+    }
 }
