2ad85f46d326066be9af715df6ff4c1fc53cf23d
Imam Mishkat <imammishkat@gmail.com>
2022-10-13 17:23:17 -0400

    its fine

--- src/Runner.java
+++ src/Runner.java
@@ -2,9 +2,18 @@ public class Runner
 {
     public static void main(String[] args)
     {
-        String greeting = "Good Morning";
-        int strLen = greeting.length();
-        System.out.println(strLen);
+        String x = "Hello there!";
+        int ind1 = x.indexOf("there");
+        String letter1 = x.substring(ind1).substring(3,3+1);
+
+        int ind2 = x.indexOf("ll");
+        String letter3 = x.substring(ind2,ind2+2);
+        String letter2 = x.substring(x.indexOf("o"), (x.indexOf("o")+1));
+
+        System.out.println("The secret letter is: \""+ letter1 +letter2 + letter3+ "\"");
+
+
+
 
     }
 }
